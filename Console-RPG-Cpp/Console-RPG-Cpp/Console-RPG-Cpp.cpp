ca33491cf1cb6cc7635a98894f03d7086c8567dd
Александр Гармаш <garmash_a@itstep.academy>
2023-11-19 14:52:36 +0200

    Colors, characters, Area, Label

--- Console-RPG-Cpp/Console-RPG-Cpp/Console-RPG-Cpp.cpp
+++ Console-RPG-Cpp/Console-RPG-Cpp/Console-RPG-Cpp.cpp
@@ -1,14 +1,30 @@
-#include <iostream>
+﻿#include <iostream>
 #include "Headers.h"
 
 int main()
 {
+    
+    Area a1(100, 25, ' ', '@');
+    a1.Move(0, 0);
+    a1.Draw();
     for (int8_t i = 0; i < 10; i++)
     {
+        Sprite s3(",,,");
+        s3.ReadColors("***");
+        s3.Move(rand() % 91 + 3, rand() % 15 + 2);
+        s3.Draw();
+        Sprite s2(" ***\n**|**");
+        s2.ReadColors(" \"\"\" \"\"$\"\"");
+        s2.Move(rand() % 91 + 2, rand() % 15 + 5);
+        s2.Draw();
         Sprite s(" ***\n*****\n *|*\n  |");
-        s.Move(rand()%100+5, rand()%25+5);
+        s.ReadColors(" \"\"\" \",\"\",\"\",D\"   D");
+        s.Move(rand()%91+6, rand()%15+6);
         s.Draw();
     }
+    Player p(10,10, "Player", " @\no+o\n |\n/ \\", 100);
+    p.sprite->Move(10, 10);
+    p.Draw();
     SetConsoleCursorPosition(hdl, { 0, 29 });
     system("pause");
 }
--- Console-RPG-Cpp/Console-RPG-Cpp/Player.h
+++ Console-RPG-Cpp/Console-RPG-Cpp/Player.h
@@ -1,3 +1,27 @@
 #pragma once
 
+class Character {
+public:
+	uint16_t hp;
+	uint16_t max_hp;
+	Sprite* sprite;
+	int32_t x;
+	int32_t y;
+	char* name; 
+	virtual void Move(int8_t dx, int8_t dy) { x += dx; y += dy; }
+	virtual void Draw() { if (sprite == nullptr) { throw "Draw nullptr sprite in baseclass Character!"; } sprite->Draw(); }
+	virtual void TakeDamage(uint8_t damage) { if (int16_t(hp) - damage <= 0) { throw "Damage in baseclass Character! HP < 0"; } hp -= damage; }
+};
 
+class Player : public Character {
+public:
+	Player(int32_t _x, int32_t _y, const char* _name, const char* _sprite, int16_t _max_hp) {
+		x = _x;  y = _y;
+		int8_t i = 0, size = 0;
+		for (const char* begin = _name; *begin != '\0'; begin++) { size++; } name = new char[size + 1];
+		for (const char* begin = _name; *begin != '\0'; begin++) { name[i] = *begin; i++; }	name[i] = '\0';
+		sprite = new Sprite(_sprite);
+		max_hp = _max_hp;
+		hp = _max_hp;
+	}
+};
